f62376ead02988c89bb784e4fcc2cb65bb6cbca2
Sealcakes <taylorcseal@gmail.com>
2023-03-24 10:54:44 -0500

    adds dashboard/login handlebars templates and dashboard/login routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,5 +5,12 @@ router.get('/', async (req, res) => {
     res.status(200).render('home');
 });
 
+router.get('/dashboard', async (req, res) => {
+    res.status(200).render('dashboard');
+});
+
+router.get('/login', async (req, res) => {
+    res.status(200). render('login');
+});
 
 module.exports = router;
