c906fc9d55ef49e8a9bc41710d317cb262c17531
Sealcakes <taylorcseal@gmail.com>
2023-04-12 23:59:21 -0500

    adds createpost and createcomment api routes and creates blogroutes in api/index.js

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Blog, User, Comment } = require('../../models');
+const { v4: uuidv4 } = require('uuid');
 
 router.post('/createpost', async (req, res) => {
     const userData = req.session.user;
@@ -9,6 +10,7 @@ router.post('/createpost', async (req, res) => {
             title: req.body.title,
             description: req.body.description,
             content: req.body.description,
+            id: uuidv4(),
             author: userData.id,
         });
 
@@ -21,5 +23,22 @@ router.post('/createpost', async (req, res) => {
 })
 
 router.post('/createcomment', async (req, res) => {
-    
-})
+    const userData = req.session.user;
+
+    try {
+        const newComment = await Comment.create({
+            id: uuidv4(),
+            content: req.body.content,
+            blog_id: req.body.blog_id,
+            user_id: userData.id,
+        });
+
+        res.status(200).json(newComment);
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
